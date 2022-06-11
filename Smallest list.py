9799535b112c76e38bb3842febaaf6b728fd5dca
Me13S <107324602+Me13S@users.noreply.github.com>
2022-06-11 17:16:33 -0400

    Add files via upload

---
+++ Smallest list.py
@@ -0,0 +1,9 @@
+def smallest (my_list): 
+    temp = 2345678
+    for x in a:
+        if x < temp:
+            temp = x
+    print (temp)
+a = [54,23,12,56,782,32,23,87,56,23,5,12,34,2,35,4,652,32,3,43]
+smallest(a)
+
